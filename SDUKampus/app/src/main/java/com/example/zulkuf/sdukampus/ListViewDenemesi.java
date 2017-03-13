2e3fb92eca9e51903111ca0ba1ec46d60fe5584d
zulkufadsiz <zlkfadsiz@ymail.com>
2017-03-14 01:29:05 +0300

    SDU Kampus Version 0.3

--- SDUKampus/app/src/main/java/com/example/zulkuf/sdukampus/ListViewDenemesi.java
+++ SDUKampus/app/src/main/java/com/example/zulkuf/sdukampus/ListViewDenemesi.java
@@ -1,10 +1,11 @@
-package com.example.zulkuf.sdukampus.fragments;
+package com.example.zulkuf.sdukampus;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,37 +14,29 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.example.zulkuf.sdukampus.ListViewDenemesi;
-import com.example.zulkuf.sdukampus.R;
+import java.util.ArrayList;
 
 /**
- * Created by zulkuf on 07/03/17.
+ * Created by zulkuf on 13/03/17.
  */
 
-public class BusFragment extends Fragment {
-
+public class ListViewDenemesi extends AppCompatActivity {
     private String[] time  = {"Dört","Sekiz","Onİki","OnSekiz","OtuzYedi","KırkAltı","KırkYedi"};
     private String[] location  = {"Batı","Tıp","Doğu","Tönge","Mevlana","Batıkent","Halıkent"};
     private String[] numbers  = {"4","8","12","18","37","46","47"};
     private ListView mBusList;
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.bus_fragment, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        mBusList = (ListView)view.findViewById(R.id.busListView);
-        MyArrayAdapter myArrayAdapter = new MyArrayAdapter(getContext());
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deneme);
+        mBusList = (ListView)findViewById(R.id.lv1);
+      MyArrayAdapter myArrayAdapter = new MyArrayAdapter(ListViewDenemesi.this);
 
         mBusList.setAdapter(myArrayAdapter);
-
     }
 
-    private class MyArrayAdapter extends ArrayAdapter<String> {
+    private class MyArrayAdapter extends ArrayAdapter<String>{
         public MyArrayAdapter(@NonNull Context context) {
             super(context, R.layout.custom_bus_list_item,time);
         }
@@ -56,7 +49,7 @@ public class BusFragment extends Fragment {
         @NonNull
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View view  = layoutInflater.inflate(R.layout.custom_bus_list_item,parent,false);
 
             ImageView img = (ImageView)view.findViewById(R.id.ivBusNumberImage);
@@ -73,4 +66,6 @@ public class BusFragment extends Fragment {
             return view;
         }
     }
+
+
 }
