cc0c40e3f77d56a38d830f4e79ec10a427267ce1
zulkufadsiz <zlkfadsiz@ymail.com>
2017-04-28 16:20:49 +0300

    SDU Kampus Version 0.73 bus content has completed

--- SDUKampus/app/src/main/java/com/example/zulkuf/sdukampus/BusItemClickActivity.java
+++ SDUKampus/app/src/main/java/com/example/zulkuf/sdukampus/BusItemClickActivity.java
@@ -3,61 +3,24 @@ package com.example.zulkuf.sdukampus;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
-import com.example.zulkuf.sdukampus.fragments.DayKey;
 
 /**
  * Created by YunusEmre on 24.4.2017.
  */
 
-public class BusItemClickActivity extends AppCompatActivity{
+public class BusItemClickActivity extends AppCompatActivity {
 
-    private ImageView imClick;
     public TextView tv1Click, tv2Click, tv3Click;
-    private String busClickTime;
-    private String busClickNumber;
-    private String busClickRoute;
-
-    public BusItemClickActivity(String[] info){
-        busClickNumber = info[0].toString();
-        busClickTime = info[1].toString();
-        busClickRoute = info[2].toString();
-    }
-    public BusItemClickActivity(){
-
-
-    }
-
-
-
-    /*public String getBusClickTime() {
-        return busClickTime;
-    }
-
-    public void setBusClickTime(String busClickTime) {
-        this.busClickTime = busClickTime;
-    }
-
-    public String getBusClickNumber() {
-        return busClickNumber;
-    }
-
-    public void setBusClickNumber(String busClickNumber) {
-        this.busClickNumber = busClickNumber;
-    }
-
-    public String getBusClickRoute() {
-        return busClickRoute;
-    }
-
-    public void setBusClickRoute(String busClickRoute) {
-        this.busClickRoute = busClickRoute;
-    }*/
-
+    RelativeLayout relative;
 
 
     @Override
@@ -65,31 +28,39 @@ public class BusItemClickActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bus_item_click_activity);
 
-        imClick = (ImageView) findViewById(R.id.ivBusClickNumberImage);
+        relative = (RelativeLayout) findViewById(R.id.rlBusItemContent);
         tv1Click = (TextView) findViewById(R.id.tvBusClickNumber);
         tv2Click = (TextView) findViewById(R.id.tvBusClickTime);
-        tv3Click = (TextView) findViewById(R.id.tvBusClickRoute);
+
 
         Bundle texts = getIntent().getExtras();
-        tv1Click.setText(texts.getString("NUMBER"));
-        tv2Click.setText(texts.getString("TIME"));
-        tv3Click.setText(texts.getString("ROUTE"));
+        String route = (String) texts.get("ROUTE");
+        String number = (String) texts.get("NUMBER");
+        String time = (String) texts.get("TIME");
+        String[] routes = route.split("-");
+
+        tv1Click.setText(number);
+        tv2Click.setText(time);
+        for (int i = 0; i < routes.length; i++) {
+
+            TextView tv = new TextView(this);
+            tv.setId(i);
+
+            RelativeLayout.LayoutParams lp = new RelativeLayout
+                    .LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+            if (i >= 0) {
+                lp.addRule(RelativeLayout.BELOW, i - 1);
+            }
+            tv.setPadding(50,30,0,0);
+            tv.setLayoutParams(lp);
+            tv.setText("* " + routes[i]);
+            relative.addView(tv);
+
+        }
+
+
+    }
 
 }
-
-
-
-
-    /*public void busInformation(String[] info){
-         setBusClickNumber(info[0].toString());
-        busClickTime = info[1].toString();
-        busClickRoute = info[2].toString();
-
-
-
-    }*/
-
-
-}
-
-
